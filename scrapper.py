05be7b246980664eff81dfc6962a4d673f33ecb0
Orion Montoya Correa <orionmontoyac@gmail.com>
2023-03-12 23:10:08 -0500

    OOP version to improve readability of the code

--- scrapper.py
+++ scrapper.py
@@ -1,18 +1,17 @@
 import requests
 import bs4
-import glob
+import logging
 import os
-"""
-<script src="js/UserActions.30ee83ef27eafec0be61.js"></script>
-<script src="js/MarkComplete.e9560adcebc4ad54e6bf.js"></script>
-<script src="js/Auth.44e05080311528b179c7.js"></script>
-
-<script src="js/Misc.a66f8a686e276f997313.js"></script>
-<script src="js/4826.ea570b7100e8c5e53e11.js"></script>
-<script src="js/7540.b7f3ab16c1d7d344980b.js"></script>
-"""
 
 from translate import Translator
+
+# Logs config
+logger = logging.getLogger(__name__)
+c_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+c_handler.setFormatter(fmt=formatter)
+logger.addHandler(c_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def group(list_, size):
@@ -30,139 +29,120 @@ def tag_visible(element):
     return True
 
 
-def text_translation_from_html(html_soup):
-    translator = Translator()
+class HomePage:
+    def __init__(self, host: str = 'https://www.classcentral.com/', file_name: str = None):
+        self.host = host
+        self.file_name = file_name
+        self.css_link_selector = 'body main section a'
+        self.html_soup = self._get_html()
 
-    texts = html_soup.findAll(string=True)
-    visible_texts = list(filter(tag_visible, texts))
+    def get_links(self):
+        # Select links inside the selector: body main section a
+        link_elements = self.html_soup.select(self.css_link_selector)
 
-    visible_texts = group(visible_texts, 40)
+        link_elements = [link_url['href'] for link_url in link_elements]
 
-    for elements in visible_texts:
-        texts_to_translate = [str(element) for element in elements]
-        translated_texts = translator.make_translation(texts=texts_to_translate)
+        # Build links
+        for index, link_to_build in enumerate(link_elements):
+            if 'http' not in link_to_build:
+                link_elements[index] = 'https://www.classcentral.com' + link_to_build
 
-        for element, translated_text in zip(elements, translated_texts):
-            element.string.replace_with(translated_text['translatedText'])
-            # print(element.text, "->", translated_text['translatedText'])
+        logger.info('Links found: {}'.format(len(link_elements)))
 
-    print('Groups translated: ', len(visible_texts))
+        return link_elements
 
-    return html_soup
+    def _get_html(self):
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+        response = requests.get(self.host, headers=headers)
+
+        html_soup = bs4.BeautifulSoup(response.text, 'lxml')
+        # Add JS for functionality
+        script_tag = html_soup.new_tag('script', src='js/Auth.44e05080311528b179c7.js')
+        html_soup.html.append(script_tag)
+        script_tag = html_soup.new_tag('script', src='js/7540.b7f3ab16c1d7d344980b.js')
+        html_soup.html.append(script_tag)
+
+        logger.info('Got html for {}'.format(self.host))
+        return html_soup
+
+    def _do_translation(self):
+        texts = self.html_soup.findAll(string=True)
+        visible_texts = list(filter(tag_visible, texts))
+        visible_texts = group(visible_texts, 40)
+
+        for elements in visible_texts:
+            texts_to_translate = [str(element) for element in elements]
+            translated_texts = translator.make_translation(texts=texts_to_translate)
+
+            for element, translated_text in zip(elements, translated_texts):
+                element.string.replace_with(translated_text['translatedText'])
+                # print(element.text, "->", translated_text['translatedText'])
+
+        logger.info('Page {} translated'.format(self.file_name))
+
+    def _save_page(self):
+        path = 'web_page/' + self.file_name
+        try:
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+        except:
+            pass
+
+        file = open(path, 'w+')
+        file.write(str(self.html_soup))
+        file.close()
+
+    def do_translation_and_save(self):
+        self._do_translation()
+        self._save_page()
+
+    def main_link_internal_webpages(self):
+
+        # Select links inside the selector: body main section a
+        link_elements = self.html_soup.select('body main section a')
+
+        for link in link_elements:
+            link['href'] = link['href'].replace('https://www.classcentral.com/', '')
+            # Clean link
+            if link['href'][0] == '/':
+                link['href'] = link['href'][1:]
+            if link['href'][-1] == '/':
+                link['href'] = link['href'][:-1]
+
+            link['target'] = '_self'
+            link['href'] = link['href'] + '.html'
+            print(link['href'])
 
 
-def get_html_code(host_url: str):
-    print("Getting HTML code for: ", host_url)
+class InternalPage(HomePage):
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
-    response = requests.get(host_url, headers=headers)
+    def __init__(self, host: str = None):
+        super().__init__(host)
+        self.host = host
+        self.file_name = self._get_file_name()
 
-    return bs4.BeautifulSoup(response.text, 'html.parser')
-
-
-def get_links(html_soup: bs4.BeautifulSoup):
-    # Select links inside the selector: body main section a
-    links = html_soup.select('body main section a')
-
-    links = [link_url['href'] for link_url in links]
-
-    # Build links
-    for index, link_to_build in enumerate(links):
-        if 'http' not in link_to_build:
-            links[index] = 'https://www.classcentral.com' + link_to_build
-
-    return links
-
-
-def save_web_page(html, file_name):
-    # Save web page to file
-    # Create folder if necessary
-    if 'web_page' not in file_name:
-        file_name = 'web_page/' + file_name
-
-    try:
-        os.makedirs(os.path.dirname(file_name), exist_ok=True)
-    except:
-        pass
-
-    file = open(file_name, 'w+')
-    file.write(str(html))
-    file.close()
-
-
-def open_html_file(file_name):
-    with open(file_name, "r", encoding='utf-8') as f:
-        text = f.read()
-        return text
-
-
-def web_scrapper():
-    # Get the home page
-    host = 'https://www.classcentral.com/'
-    html = get_html_code(host_url=host)
-
-    # Add JS for functionality
-    script_tag = html.new_tag('script', src='js/Auth.44e05080311528b179c7.js')
-    html.html.append(script_tag)
-    script_tag = html.new_tag('script', src='js/7540.b7f3ab16c1d7d344980b.js')
-    html.html.append(script_tag)
-
-    # Save home page
-    save_web_page(html, 'index.html')
-
-    # Get the links from the home page
-    elements_links = get_links(html)
-
-    for i, link in enumerate(elements_links, start=1):
-        print(i, link)
-        # Getting html code for internal page
-        html = get_html_code(link)
+    def _get_file_name(self):
         # Create file name
-        file_name = link.replace('https://www.classcentral.com/','')
+        file_name = self.host.replace('https://www.classcentral.com/', '')
         if file_name[-1] == '/':
             file_name = file_name[:-1] + '.html'
         else:
             file_name += '.html'
 
-        # Save web page to file
-        save_web_page(html, file_name)
+        return file_name
 
 
-def main_link_internal_webpages():
-    text = open_html_file('web_page/index.html')
-
-    html_soup = bs4.BeautifulSoup(text, 'html.parser')
-    # Select links inside the selector: body main section a
-    links = html_soup.select('body main section a')
-
-    for link in links:
-        link['href'] = link['href'].replace('https://www.classcentral.com/','')
-        # Clean link
-        if link['href'][0] == '/':
-            link['href'] = link['href'][1:]
-        if link['href'][-1] == '/':
-            link['href'] = link['href'][:-1]
-
-        link['target'] = '_self'
-        link['href'] = link['href'] + '.html'
-        print(link['href'])
-
-    save_web_page(html_soup, 'index.html')
-
+translator = Translator()
 
 if __name__ == "__main__":
-    # Get html code from the website
-    web_scrapper()
+    # Get home page
+    home_page = HomePage(file_name='index.html')
+    links = home_page.get_links()
+    home_page.main_link_internal_webpages()
+    # Do Translation
+    home_page.do_translation_and_save()
 
-    # Link local web pages to index.html
-    main_link_internal_webpages()
-
-    # Translate de documents
-    html_files = glob.glob('web_page/**/*.html', recursive=True)
-
-    for html_file in html_files:
-        print('Page to translate: ', html_file)
-        text = open_html_file(html_file)
-        html = bs4.BeautifulSoup(text, 'html.parser')
-        html_translation = text_translation_from_html(html)
-        save_web_page(html_translation, file_name=html_file)
+    # Get internal pages
+    internal_pages = []
+    for link in links:
+        internal_page = InternalPage(host=link)
+        internal_page.do_translation_and_save()
